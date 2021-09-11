c309e5978ac43e160219cd7f24c00fd0378e04e1
Celox16 <sck04184@naver.com>
2021-09-11 14:58:48 +0900

    feat: make SubjectListAdapterTest.java
    but occurred error code 500

--- app/src/main/java/com/example/biometric_auth_attendence/ModifySugang.java
+++ app/src/main/java/com/example/biometric_auth_attendence/ModifySugang.java
@@ -7,18 +7,29 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ModifySugang extends AppCompatActivity {
-    private TextView tv_major, tv_stuNum, tv_userName;
     private Button[] weeks = new Button[5];
-    private SubjectListAdapter subjectListAdapter;
-    private List<SubjectList> listViewBtnItems;
     private ListView listView;
+    SubjectListAdapterTest subjectListAdapterTest;
+    public static ArrayList<SubjectList> subjectListArrayList = new ArrayList<>();
+    String url = "http://125.141.27.3/biometric_auth/getDBTest.php";
+    SubjectList subjectList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +37,6 @@ public class ModifySugang extends AppCompatActivity {
         setContentView(R.layout.activity_modify_sugang);
 
         // find view by values
-        tv_major = findViewById(R.id.tv_modifySugang_major);
-        tv_stuNum = findViewById(R.id.tv_modifySugang_stuNum);
-        tv_userName = findViewById(R.id.tv_modifySugang_userName);
-
         weeks[0] = findViewById(R.id.btn_modifySugang_MON);
         weeks[1] = findViewById(R.id.btn_modifySugang_TUE);
         weeks[2] = findViewById(R.id.btn_modifySugang_WED);
@@ -37,6 +44,10 @@ public class ModifySugang extends AppCompatActivity {
         weeks[4] = findViewById(R.id.btn_modifySugang_FRI);
 
         listView = findViewById(R.id.ls_modifySugang_subjectList);
+        subjectListAdapterTest = new SubjectListAdapterTest(this, subjectListArrayList);
+        listView.setAdapter(subjectListAdapterTest);
+
+        retrieveData();
 
         // get user information from before activity
         Intent intentByBeforeActivity = getIntent();
@@ -45,38 +56,47 @@ public class ModifySugang extends AppCompatActivity {
         String userName = intentByBeforeActivity.getStringExtra("userName");
         int studentNumber = intentByBeforeActivity.getIntExtra("studentNumber", 0);
 
-        // set ListView Adapter
-        subjectListAdapter = new SubjectListAdapter(getApplicationContext(), listViewBtnItems);
-        listView.setAdapter(subjectListAdapter);
+    }
 
-        // get ListView subject list data from database
-        Intent intentByDatabase = getIntent();
-        try{
-            JSONObject jsonObject = new JSONObject(intentByDatabase.getStringExtra("subjectList"));
-            JSONArray jsonArray = jsonObject.getJSONArray("response");
-            int count = 0;
-            int subjectCode;
-            String subjectName, dayOfTheWeek, professor, startTime, endTime, bluetoothName;
-            while(count<jsonArray.length()){
-                JSONObject object = jsonArray.getJSONObject(count);
-                subjectCode = object.getInt("subjectCode");
-                subjectName = object.getString("subjectName");
-                dayOfTheWeek = object.getString("dayOfTheWeek");
-                professor = object.getString("professor");
-                startTime = object.getString("startTime");
-                endTime = object.getString("endTime");
-                bluetoothName = object.getString("bluetoothName");
+    public void retrieveData(){
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        subjectListArrayList.clear();
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("data");
 
+                            if(success.equals("1")){
+                                for(int i = 0; i<jsonArray.length(); i++){
+                                    JSONObject object = jsonArray.getJSONObject(i);
 
+                                    int subjectCode = object.getInt("subjectCode");
+                                    String subjectName = object.getString("subjectName");
+                                    String dayOfTheWeek = object.getString("dayOfTheWeek");
+                                    String professor = object.getString("professor");
+                                    String startTime = object.getString("startTime");
+                                    String endTime = object.getString("endTime");
+                                    String bluetoothName = object.getString("bluetoothName");
+
+                                    subjectList = new SubjectList(subjectCode, subjectName, dayOfTheWeek, professor,startTime, endTime, bluetoothName);
+                                    subjectListArrayList.add(subjectList);
+                                    subjectListAdapterTest.notifyDataSetChanged();
+                                }
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ModifySugang.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-        // set TextView on the tab
-        tv_userName.setText(userName);
-        tv_major.setText(userMajor);
-        tv_stuNum.setText(""+studentNumber);
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
